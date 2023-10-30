161fe43014ec7312a4e9dee82744e745350b9a91
afhorton <106110920+afhorton@users.noreply.github.com>
2023-10-30 11:10:04 -0600

    Add

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(<FinalProject />);
+ReactDOM.render (
+
+    <div><h1>What's Up</h1><ul><li>Item 1</li><li>Item 2</li></ul></div>
+    ,
+    document.getElementById('root')
+)
