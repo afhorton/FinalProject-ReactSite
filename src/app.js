958759b605f4efa722b2ee75739ee5429adc26af
afhorton <106110920+afhorton@users.noreply.github.com>
2023-10-31 11:33:55 -0600

    Here

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
-ReactDOM.render (
+import React from "react";
+import { useForm } from "react-hook-form";
+import './App.css';
 
-    <div><h1>What's Up</h1><ul><li>Item 1</li><li>Item 2</li></ul></div>
-    ,
-    document.getElementById('root')
-)
+function App() {
+  const { register, handleSubmit, formState: { errors }} = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <>
+      <p className="title">Login</p>
+
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("name")} />
+        <input type="email" {...register("email", { required: true })} />
+
+        {errors.email && (
+          <span style={{ color: "red" }}>*Email* is mandatory </span>
+        )}
+        <input type="password" {...register("password")} />
+        <input type={"submit"} />
+      </form>
+    </>
+  );
+}
+
+export default App;
